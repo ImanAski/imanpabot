7e43a6aeaaffdc0038621edaba3f239b47a52153
Iman Mokhtari Aski <imanskater@outlook.com>
2020-05-22 22:36:50 +0430

    removed token

--- bot.js
+++ bot.js
@@ -9,4 +9,4 @@ client.on('message', message => {
     message.reply('I am hearing your message here it is -> '.message.content);
 });
 
-client.login('NzEzNDQ3NjY0OTA5Mjg3NTU1.XsgQcA.67mOVIIVcollUUQou9eIE_Pt_gw');
+client.login(process.env.BOT_TOKEN);
