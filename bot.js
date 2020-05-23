c76581e9386b03a99498bae19ee893c265433ae0
Iman Mokhtari Aski <imanskater@outlook.com>
2020-05-23 20:02:35 +0430

    fixed typo error

--- bot.js
+++ bot.js
@@ -1,22 +1,14 @@
 const Discord = require('discord.js');
-const client = new Discord.client();
+const client = new Discord.Client();
 
-client.on('message', message => {
-    // If the message is "how to embed"
-    if (message.content === 'how to embed') {
-      // We can create embeds using the MessageEmbed constructor
-      // Read more about all that you can do with the constructor
-      // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-      const embed = new MessageEmbed()
-        // Set the title of the field
-        .setTitle('A slick little embed')
-        // Set the color of the embed
-        .setColor(0xff0000)
-        // Set the main content of the embed
-        .setDescription('Hello, this is a slick embed!');
-      // Send the embed to the same channel as the message
-      message.channel.send(embed);
-    }
-  });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
