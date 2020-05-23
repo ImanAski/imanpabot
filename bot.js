1fa3cdc114a40b9ee53079d71ab8460c1edac120
Iman Mokhtari Aski <imanskater@outlook.com>
2020-05-23 21:46:37 +0430

    removed pachekhari

--- bot.js
+++ bot.js
@@ -6,15 +6,11 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if(msg.content.startsWith("/pachekhari")) {
-    msg.reply("بسیار زیبا بود جناب/سرکارخانم "+message.author);
-  }
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 try {
-  console.log(process.env.BOT_TOKEN);
   client.login(process.env.BOT_TOKEN);
 } catch(e) {
   console.log("This is the error -> ".e);
